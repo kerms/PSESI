e63c2531cb319eaafb5d8916cb60c49357b00873
Paul-EmmanuelB <paulemmanuelb@gmail.com>
2019-04-24 18:41:22 +0200

    Maj et finalisatin du Flex. Compris, expliqué, fonctionnel et nécessitera des améliorations si on a la temps

--- flex/flex.ino
+++ flex/flex.ino
@@ -16,15 +16,17 @@ Arduino 1.6.7
 ******************************************************************************/
 const int FLEX_PIN = 4; // Pin connected to voltage divider output
 
-// Measure the voltage at 5V and the actual resistance of your
-// 47k resistor, and enter them below:
+
+//POUR AMELIORER :
+// Measure the voltage at your pin and the actual resistance of your
+// choice you want to use, and enter them below:
 const float VCC = 3.3; // Measured voltage of Ardunio 5V line
-const float R_DIV = 10500.0; // Measured resistance of 3.3k resistor
+const float R_DIV = 10000.0; // Measured resistance you choose
 
 // Upload the code, then try to adjust these values to more
 // accurately calculate bend degree.
-const float STRAIGHT_RESISTANCE = 0.0; // resistance when straight
-const float BEND_RESISTANCE = 90000.0; // resistance at 90 deg
+const float STRAIGHT_RESISTANCE = 10000.; // resistance when straight 4' : 10 kOhms _ 2'2 : 25 kOhms
+const float BEND_RESISTANCE = 20000.0; // resistance at 90 deg     4' : 20 kOhms _ 2'2 : 50 kOhms
 
 void setup() 
 {
@@ -36,14 +38,14 @@ void loop()
 {
   // Read the ADC, and calculate voltage and resistance from it
   int flexADC = analogRead(FLEX_PIN);
-  float flexV = flexADC * VCC / 1023.0;
-  float flexR = R_DIV * (VCC / flexV - 1.0);
+  Serial.println(String(flexADC));
+  float flexV = flexADC * VCC / 4095.0; // Produit en croix pour obtenir la valeur en Volt au pin choisi sachant que pour 3.3V on a une val au pin de 4095
+  float flexR = R_DIV * ( (VCC / flexV) - 1.0); // Exploitation du diviseur de tension pour obetenir la valeur de resistance du flex sensor " flexV = Vcc*flexR / (flexR + R_DIV) " 
   Serial.println("Resistance: " + String(flexR) + " ohms");
 
   // Use the calculated resistance to estimate the sensor's
   // bend angle:
-  float angle = map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
-                   0, 90.0);
+  float angle = map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE,0, 90.0); // Remappe la valeur de la resistance allant d'une valeur à une autre ( Straigth to Bend ) sur celle d'une valeur d'angle ( 0 à 90 °)
   Serial.println("Bend: " + String(angle) + " degrees");
   Serial.println();
 
